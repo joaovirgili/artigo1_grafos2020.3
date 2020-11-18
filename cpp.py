2e93694011eaf92e2c198d7b7b796589dff1bc0a
Joanderson <joanderson.rikudo@yahoo.com>
2020-11-18 20:03:23 -0300

    adicionado arestas à árvore

--- cpp.py
+++ cpp.py
@@ -43,6 +43,9 @@ class Vertice:
     def __str__(self):
         return "V({0})".format(self.id)
 
+    def __eq__(self, obj):
+        return isinstance(obj, Vertice) and obj.id == self.id
+
 
 class Node:
     # pai = Node()
@@ -52,12 +55,29 @@ class Node:
     def __init__(self, vertices, arestas):
         self.vertices = vertices
         self.arestas = arestas
+        self.verticesNaoConectados = vertices
 
+    def __eq__(self, obj):
+        if not isinstance(obj, Node):
+            return False
+        self.vertices = sorted(self.vertices, key=id)
+        obj.vertices = sorted(obj.vertices, key=id)
+        #self.arestas = sorted(self.arestas, key=id)
+        #obj.arestas = sorted(obj.arestas, key=id)
+        return obj.vertices == self.vertices #and obj.arestas == self.arestas
+
+class ArestaArvore:
+    def __init__(self, no1, no2):
+        self.no1 = no1
+        self.no2 = no2
 
 class Arvore:
-    def __init__(self, root, largura):
-        self.root = root
-        self.largura = largura
+    nos = []
+    arestasArvore = []
+    # def __init__(self):
+        # self.root = root
+        # self.largura = largura
+
 
 
 def vizinhosDoVertice(grafo, verticeId, outroId):
@@ -133,9 +153,9 @@ def temTodasAsArestas(nos, arestas):
     return True
 
 
-def printArvore(nos):
+def printArvore(arvore):
     texto = ""
-    for no in nos:
+    for no in arvore.nos:
         textoNo = ""
         for aresta in no.arestas:
             textoNo = "{0}{1} | ".format(textoNo, aresta)
@@ -145,7 +165,8 @@ def printArvore(nos):
 
 
 def deveAdicionar(novoNo, nos):
-
+    if len(nos) == 0:
+        return True
     for aresta in novoNo.arestas:
         flag = True
         for no in nos:
@@ -156,20 +177,120 @@ def deveAdicionar(novoNo, nos):
             return True
     return False
 
+def existeNoPrecisaConectar(no, nos):
+    return no
+
+
+def calculaArestasArvore(nos):
+    arestasNos = []
+    noArbitrario = nos[random.randint(0, len(nos))]
+    noConectar = existeNoPrecisaConectar(noArbitrario, nos)
+    # if noConectar is not None:
+        #conecta o nó e finaliza a rodada
+        # arestasNos.append()
+        #continue
+    return []
+
+class NodesContendoVertice:
+    def __init__(self, node, vertices):
+        self.node = node
+        self.vertices = vertices
+
+    def __eq__(self, obj):
+        return isinstance(obj, NodesContendoVertice) and obj.node == self.node
+
+    def addVertice(self, vertice):
+        if not vertice.__class__ == Vertice.__class__:
+            return
+        self.vertices.append(vertice)
+
+def getQuantidadeArestasNoArvore(node, arvore):
+    cont = 0
+    for aresta in arvore.arestasArvore:
+        if aresta.no1 == node or aresta.no2 == node:
+            cont = cont + 1
+    return cont
+
+
+def criaArestaNaArvore(noNovo, nos, arvore):
+
+    #adiciono o noNovo como filho de uma das tais interseções.
+
+    #para cada vértice v em noNovo, busco na árvore todos os Nós com aquele vértice
+    listaNodesContendoVertices = []
+    for v in noNovo.vertices:
+        for no in nos:
+            for ver in no.vertices:
+                if v == ver:
+                    n = NodesContendoVertice(no, [v])
+                    if listaNodesContendoVertices.__contains__(n):
+                        for item in listaNodesContendoVertices:
+                            if item == no:
+                                item.addVertice(v)
+                                break
+                    else:
+                        listaNodesContendoVertices.append(n)
+                    break
+        #listaNodesContendoVertices.append(NodesContendoVertice(nosComV, v))
+    if len(listaNodesContendoVertices) == 0:
+        return None #not possible to find any node with any vertex v of noNovo
+
+    #encontro então a maior interseção entre todos os resultados das buscas para atender o méximo de vértices v de noNovo
+    maior = 0
+    indexParaNoPai = -1
+    for i in range(len(listaNodesContendoVertices)):
+        nodeComVertices = listaNodesContendoVertices.__getitem__(i)
+        if nodeComVertices.node.__eq__(noNovo):
+            continue
+        if getQuantidadeArestasNoArvore(nodeComVertices.node, arvore) >= 3:
+            continue
+        qtdVertices = len(nodeComVertices.vertices)
+        if maior < qtdVertices:
+            maior = qtdVertices
+            indexParaNoPai = i
+
+    if indexParaNoPai == -1:
+        return None
+    #agora eu adiciono o no indexado por indexParaNoPai como pai de noNovo
+    aresta = ArestaArvore(noNovo, nos.__getitem__(indexParaNoPai))
+
+    # print("Nó 1: vértices: ")
+    # for a in aresta.no1.vertices:
+    #     print(a)
+    return aresta
+
+
 
 def tree_decomposition(k, grafo):
-    # arvore = Arvore()
-    nos = []
+    arvore = Arvore()
+    nosLivres = []
     # print(temTodosOsVertices(nos, grafo.vertices))
     # print(temTodasAsArestas(nos, grafo.arestas))
     while (True):
         no = criarNo(k, grafo)
-        if len(nos) == 0 or deveAdicionar(no, nos):
-            nos.append(no)
-            if temTodosOsVertices(nos, grafo.vertices) and temTodasAsArestas(nos, grafo.arestas):
-                break
+        if not deveAdicionar(no, arvore.nos):
+            continue
+        arvore.nos.append(no)
+        nosLivres.append(no)
+        for no in nosLivres:
+            aresta = criaArestaNaArvore(no, arvore.nos, arvore)
+            if aresta is not None:
+                arvore.arestasArvore.append(aresta)
+                nosLivres.remove(no)
 
-    printArvore(nos)
+        if temTodosOsVertices(arvore.nos, grafo.vertices) and temTodasAsArestas(arvore.nos, grafo.arestas):
+            for no in nosLivres:
+                aresta = criaArestaNaArvore(no, arvore.nos, arvore)
+                if aresta is not None:
+                    arvore.arestasArvore.append(aresta)
+                    nosLivres.remove(no)
+            if len(nosLivres) == 0:
+                break
+            else:
+                print("Erro: não foi possível criar uma árvore. Tente novamente.")
+
+    printArvore(arvore)
+    #arestas = calculaArestasArvore(nosLivres)
 
 
 v1 = Vertice("v1")
